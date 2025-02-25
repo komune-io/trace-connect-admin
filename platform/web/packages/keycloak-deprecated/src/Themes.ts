fc49716295b1e2af3194794e215c921f96265058
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-02-25 13:47:27 +0100

    chore(keycloak-deprecated): remove deprecated Keycloak theme package and its related files to streamline the project The entire `keycloak-deprecated` package is removed as it is no longer needed, which helps to reduce clutter and maintain a cleaner codebase. This change also eliminates outdated files and dependencies that may cause confusion or maintenance overhead in the future.

--- platform/web/packages/keycloak-deprecated/src/Themes.ts
+++
@@ -1,8 +0,0 @@
-import { Theme as AruiTheme} from "@komune-io/g2";
-import { DeepPartial } from "@komune-io/g2";
-
-export const theme: DeepPartial<AruiTheme> = {// to complete and to use
-  colors: {
-    background: "#FAF8F3"
-  }
-};
